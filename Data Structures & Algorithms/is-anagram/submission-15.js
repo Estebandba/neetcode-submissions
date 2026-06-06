76ba802e9cbc9d89c3977702454f531f15bbbef1
Esteban <estebandba20@gmail.com>
2026-06-06 14:03:05 +0100

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        const sMap = new Map()
+        const tMap = new Map()
+
+        if(s.length !== t.length) return false;
+
+        for(let i = 0; i < s.length; i++){
+            sMap.set(s[i], (sMap.get(s[i]) || 0) + 1)
+            tMap.set(t[i], (tMap.get(t[i]) || 0) + 1)
+        }
+
+        for(let i = 0; i < s.length; i++){
+            if(sMap.get(s[i]) !== tMap.get(s[i])){
+                return false
+            }
+        }
+
+        return true;
+    }
+}
