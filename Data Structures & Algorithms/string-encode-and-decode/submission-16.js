d1c72bf266b444818519e49c1c3d251f5ad8f934
Esteban <estebandba20@gmail.com>
2026-06-06 18:36:10 +0100

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encodedString = ''
+        for(const str of strs){
+            encodedString += `${str.length}#${str}`
+        }
+        return encodedString
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const result = []
+
+        let i = 0;
+        let j = 0;
+
+        while(i < str.length){
+            j = i;
+
+            while(str[j] !== '#'){
+                j++
+            }
+
+            // 3#cat
+            const lengthString = parseInt(str.substring(i, j))
+            i = j + 1;
+            const stringValue = str.substring(i, i + lengthString)
+            result.push(stringValue)
+
+            i = i + lengthString;
+        }
+
+        return result;
+    }
+}
